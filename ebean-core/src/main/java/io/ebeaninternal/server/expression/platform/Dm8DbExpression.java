7e4bf54dac147945a8da8d89f3199fae45629f72
李清波 <1991tss@gmail.com>
2025-01-17 16:57:59 +0800

    dm8 json query

--- ebean-core/src/main/java/io/ebeaninternal/server/expression/platform/Dm8DbExpression.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/expression/platform/Dm8DbExpression.java
@@ -1,5 +1,7 @@
 package io.ebeaninternal.server.expression.platform;
 
+import io.ebeaninternal.server.expression.Op;
+
 /**
  * Postgres JSON and ARRAY expression handler
  */
@@ -17,6 +19,27 @@ final class Dm8DbExpression extends BasicDbExpression {
       }
     }
     if (!contains) {
+      request.append(')');
+    }
+  }
+
+  @Override
+  public void json(DbExpressionRequest request, String propName, String path, Op operator, Object value) {
+    String[] paths = path.split("\\.");
+    request.property(propName);
+    for (String _path : paths) {
+      request.append(" ->> '").append(_path).append("'");
+    }
+    request.append(operator.bind());
+  }
+
+  @Override
+  public void arrayIsEmpty(DbExpressionRequest request, String propName, boolean empty) {
+    if (empty) {
+      request.append("not (");
+    }
+    request.append("json_contains_path(replace(" + propName + ",'\"',''),'ONE','$[0]')");
+    if (!empty) {
       request.append(')');
     }
   }
